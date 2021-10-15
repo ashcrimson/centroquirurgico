efdb018c06f539b0480a3e4428f0eccacc1e4c81
Edras Altamirano <altamiranoesdras@gmail.com>
2021-10-15 16:24:11 -0600

    Fix migraciones para oracle

--- database/migrations/2021_10_12_224833_add_foreign_keys_to_partes_table.php
+++ database/migrations/2021_10_12_224833_add_foreign_keys_to_partes_table.php
@@ -14,16 +14,16 @@ class AddForeignKeysToPartesTable extends Migration
     public function up()
     {
         Schema::table('partes', function (Blueprint $table) {
-            $table->foreign('cirugia_tipo_id', 'fk_partes_cirugia_tipos')->references('id')->on('cirugia_tipos')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('clasificacion_id', 'fk_partes_clasificaciones1')->references('id')->on('clasificaciones')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('diagnostico_id', 'fk_partes_diagnosticos1')->references('id')->on('diagnosticos')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('especialidad_id', 'fk_partes_especialidades1')->references('id')->on('especialidades')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('intervencion_id', 'fk_partes_intervenciones1')->references('id')->on('intervenciones')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('pabellon_id', 'fk_partes_pabellones1')->references('id')->on('pabellones')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('paciente_id', 'fk_partes_pacientes1')->references('id')->on('pacientes')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('estado_id', 'fk_partes_parte_estados1')->references('id')->on('parte_estados')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('preoperatorio_id', 'fk_partes_preoperatorios1')->references('id')->on('preoperatorios')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-            $table->foreign('user_ingresa', 'fk_partes_users1')->references('id')->on('users')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+            $table->foreign('cirugia_tipo_id', 'fk_partes_cirugia_tipos')->references('id')->on('cirugia_tipos');
+            $table->foreign('clasificacion_id', 'fk_partes_clasificaciones1')->references('id')->on('clasificaciones');
+            $table->foreign('diagnostico_id', 'fk_partes_diagnosticos1')->references('id')->on('diagnosticos');
+            $table->foreign('especialidad_id', 'fk_partes_especialidades1')->references('id')->on('especialidades');
+            $table->foreign('intervencion_id', 'fk_partes_intervenciones1')->references('id')->on('intervenciones');
+            $table->foreign('pabellon_id', 'fk_partes_pabellones1')->references('id')->on('pabellones');
+            $table->foreign('paciente_id', 'fk_partes_pacientes1')->references('id')->on('pacientes');
+            $table->foreign('estado_id', 'fk_partes_parte_estados1')->references('id')->on('parte_estados');
+            $table->foreign('preoperatorio_id', 'fk_partes_preoperatorios1')->references('id')->on('preoperatorios');
+            $table->foreign('user_ingresa', 'fk_partes_users1')->references('id')->on('users');
         });
     }
 
