45166cf86e2fdb6a01e54492afe0d0c5de46acfa
Edras Altamirano <altamiranoesdras@gmail.com>
2021-10-21 19:41:35 -0600

    nuevas migraciones

---
+++ database/migrations/2021_10_21_193718_create_bitacoras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBitacorasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitacoras', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parte_id')->index('fk_bitacoras_idx1');
+            $table->unsignedBigInteger('user_id')->index('fk_bitacoras_idx2');
+            $table->string('titulo')->nullable();
+            $table->text('descripcion');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitacoras');
+    }
+}
---
+++ database/migrations/2021_10_21_193719_add_foreign_keys_to_bitacoras_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToBitacorasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bitacoras', function (Blueprint $table) {
+            $table->foreign('parte_id', 'fk_bitacoras_partes1')->references('id')->on('partes');
+            $table->foreign('user_id', 'fk_bitacoras_users1')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bitacoras', function (Blueprint $table) {
+            $table->dropForeign('fk_bitacoras_partes1');
+            $table->dropForeign('fk_bitacoras_users1');
+        });
+    }
+}
