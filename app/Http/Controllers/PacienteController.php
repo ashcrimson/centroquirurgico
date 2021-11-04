01435ee19b923d73a86f0cbb0e0caf186902767e
Edras Altamirano <altamiranoesdras@gmail.com>
2021-11-03 21:53:02 -0600

    Mejoras PacienteController.php

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -178,7 +178,7 @@ class PacienteController extends AppBaseController
         /**
          * @var Paciente $paciente
          */
-        $paciente = Paciente::with('solicitudes')->where('run',$request->run)->first();
+        $paciente = Paciente::with('partes')->where('run',$request->run)->first();
 
 
         if ($paciente){
