9c788e8991ed98749950540057f223e9266cdc16
Edras Altamirano <altamiranoesdras@gmail.com>
2021-11-19 21:50:45 -0600

    Nuevas migraciones

---
+++ database/migrations/2021_11_19_214852_create_parte_examenes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParteExamenesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parte_examenes', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->unsignedBigInteger('parte_id')->index('fk_parte_examenes_idx1');
+            $table->unsignedBigInteger('examen_id')->index('fk_parte_examenes_idx2');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parte_examenes');
+    }
+}
