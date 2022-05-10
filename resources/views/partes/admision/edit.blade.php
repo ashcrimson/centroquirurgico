db7181085e707eed4a2b95c386351808e5740208
Josue Martinez <m.josue3443@gmail.com>
2022-05-10 15:35:37 -0600

    se quita boton guardar para el perfil admision

--- resources/views/partes/admision/edit.blade.php
+++ resources/views/partes/admision/edit.blade.php
@@ -58,9 +58,9 @@
                                             Cancelar
                                         </a>
                                         &nbsp;
-                                        <button type="submit" class="btn btn-outline-success">
-                                            <i class="fa fa-floppy-o"></i> Guardar
-                                        </button>
+{{--                                        <button type="submit" class="btn btn-outline-success">--}}
+{{--                                            <i class="fa fa-floppy-o"></i> Guardar--}}
+{{--                                        </button>--}}
 
                                         <button type="submit" class="btn btn-outline-primary ml-2" value="1" name="lista_espera">
                                             <i class="fa fa-floppy-o"></i> Ingresar a lista de espera
