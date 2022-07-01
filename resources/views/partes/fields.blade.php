0ee6b403f2218f80a12c23c0c987706bacf84cfe
Josue Martinez <m.josue3443@gmail.com>
2022-06-30 23:02:09 -0600

    se cambia icono para descargar de icono consetimiento

--- resources/views/partes/fields.blade.php
+++ resources/views/partes/fields.blade.php
@@ -467,7 +467,7 @@
                     {!! Form::label('consentimiento', 'Consentimiento informado, firmado y archivado en ficha clínica:') !!}
 
                     <a href="http://acreditacion.hospitalnaval.cl/index.php?option=com_content&view=article&id=50&Itemid=72&dir=JSROOT%2FConsentimientos/consentimientos">
-                        <i class="fas fa-file" style="font-size:20px;"></i></a>
+                        <i class="fas fa-download" style="font-size:20px;"></i></a>
                     <input type="checkbox" data-toggle="toggle" data-size="normal" data-on="Si" data-off="No" data-style="ios" name="consentimiento" id="consentimiento"
                             value="1"
                         {{ ($parte->consentimiento ?? old('consentimiento') ?? false) ? 'checked' : '' }}>
