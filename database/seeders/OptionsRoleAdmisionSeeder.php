6a4f5c741f22308d5dd53f13724bc3d9fa8dcff8
Josue Martinez <m.josue3443@gmail.com>
2022-07-05 01:03:43 -0600

    se agrego permisos para el role admision

---
+++ database/seeders/OptionsRoleAdmisionSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class OptionsRoleAdmisionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        /**
+         * @var Role $role
+         */
+        $role = Role::find(Role::ADMISION);
+
+        // MANTENEDORES
+        $role->options()->syncWithoutDetaching([
+            22, //partes admision
+            14, //Pacientes
+            29, //Medicamentos
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            23,
+            24,
+            25,
+            26,
+            27,
+            28,
+            32,
+            33,
+        ]);
+
+    }
+}
