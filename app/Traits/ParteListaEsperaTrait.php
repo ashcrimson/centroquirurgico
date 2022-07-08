9f9ca1d7d2ee832837401839ecfed76afd8cdc10
Josue Martinez <m.josue3443@gmail.com>
2022-07-08 14:31:48 -0600

    se valida en lista espera, dependiendo que control eligan, tiene que validar que los preop haga su validacion, cuando la hagan se mostrara de color verde el boton preop en lista de espera

---
+++ app/Traits/ParteListaEsperaTrait.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Parte;
+
+trait ParteListaEsperaTrait
+{
+
+    public function validarPreopColor(Parte $parte)
+    {
+
+        $attr = null;
+
+        if ($parte->control_preop_eu && $parte->control_preop_anestesista && $parte->control_preop_medico &&
+            $parte->control_banco_sangre) {
+            if ($parte->pase_preop_eu && $parte->pase_preop_anestesista && $parte->pase_preop_medico &&
+                $parte->pase_banco_sagre) {
+                $attr = '#99E066';
+            }
+        }
+
+        if ($parte->control_preop_eu && ($parte->control_preop_anestesista || $parte->control_preop_medico ||
+                                         $parte->control_banco_sangre)) {
+            if ($parte->pase_preop_eu && ($parte->pase_preop_anestesista || $parte->pase_preop_medico ||
+                                          $parte->pase_banco_sagre)) {
+                $attr = '#99E066';
+            }
+        }
+
+        if ($parte->control_preop_anestesista && ($parte->control_preop_eu || $parte->control_preop_medico ||
+                                                  $parte->control_banco_sangre)) {
+            if ($parte->pase_preop_anestesista && ($parte->pase_preop_eu || $parte->pase_preop_medico ||
+                                                   $parte->pase_banco_sagre)) {
+                $attr = '#99E066';
+            }
+        }
+
+        if ($parte->control_preop_medico && ($parte->control_preop_anestesista || $parte->control_preop_eu ||
+                                             $parte->control_banco_sangre)) {
+            if ($parte->pase_preop_medico && ($parte->pase_preop_anestesista || $parte->pase_preop_eu ||
+                                              $parte->pase_banco_sagre)) {
+                $attr = '#99E066';
+            }
+        }
+
+        if ($parte->control_banco_sangre && ($parte->control_preop_medico  || $parte->control_preop_anestesista ||
+                                             $parte->control_preop_eu)) {
+            if ($parte->pase_banco_sagre && ($parte->pase_preop_medico || $parte->pase_preop_anestesista ||
+                                             $parte->pase_preop_eu)){
+                $attr = '#99E066';
+            }
+        }
+
+        return $attr;
+
+    }
+
+}
