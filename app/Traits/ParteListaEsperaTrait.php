d0425f9ddea79bb2bc56a5c7ccb6e66c5ee44489
Josue Martinez <m.josue3443@gmail.com>
2022-07-09 00:00:47 -0600

    se agrega validaciones para preop

--- app/Traits/ParteListaEsperaTrait.php
+++ app/Traits/ParteListaEsperaTrait.php
@@ -20,119 +20,113 @@ trait ParteListaEsperaTrait
             }
         }
 
-        if ($parte->control_preop_eu && ($parte->control_preop_anestesista && $parte->control_preop_medico &&
-                                         $parte->control_banco_sangre)) {
-            if ($parte->pase_preop_eu && ($parte->pase_preop_anestesista && $parte->pase_preop_medico &&
-                                          $parte->pase_banco_sagre)) {
+        if ($parte->control_preop_eu && $parte->control_preop_anestesista && !$parte->control_preop_medico &&
+            !$parte->control_banco_sangre) {
+            if ($parte->pase_preop_eu && $parte->pase_preop_anestesista && !$parte->pase_preop_medico &&
+                !$parte->pase_banco_sagre) {
+                $attr = '#99E066';
+            }
+        } else if ($parte->control_preop_eu && $parte->control_preop_medico && !$parte->control_preop_anestesista &&
+                   !$parte->control_banco_sangre) {
+            if ($parte->pase_preop_eu && $parte->pase_preop_medico && !$parte->pase_preop_anestesista &&
+                !$parte->pase_banco_sagre) {
+                $attr = '#99E066';
+            }
+        } else if ($parte->control_preop_eu && $parte->control_banco_sangre && !$parte->control_preop_anestesista  &&
+                   !$parte->control_preop_medico) {
+            if ($parte->pase_preop_eu && $parte->pase_banco_sagre && !$parte->pase_preop_anestesista &&
+                !$parte->pase_preop_medico) {
+                $attr = '#99E066';
+            }
+        } else if ($parte->control_preop_eu && !$parte->control_preop_anestesista  && !$parte->control_preop_medico &&
+                   !$parte->control_banco_sangre) {
+            if ($parte->pase_preop_eu && !$parte->pase_preop_anestesista && !$parte->pase_preop_medico &&
+                !$parte->pase_banco_sagre) {
                 $attr = '#99E066';
             }
         }
 
-        if ($parte->control_preop_eu && ($parte->control_preop_anestesista)) {
-            if ($parte->pase_preop_eu && ($parte->pase_preop_anestesista)) {
-                $attr = '#99E066';
-            }
-        }
+//        if ($parte->control_preop_anestesista && $parte->control_preop_eu && $parte->control_preop_medico &&
+//                                                  $parte->control_banco_sangre) {
+//            if ($parte->pase_preop_anestesista && $parte->pase_preop_eu && $parte->pase_preop_medico &&
+//                                                   $parte->pase_banco_sagre) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_preop_anestesista && ($parte->control_preop_eu)) {
+//            if ($parte->pase_preop_anestesista && ($parte->pase_preop_eu)) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_preop_anestesista && $parte->control_preop_medico) {
+//            if ($parte->pase_preop_anestesista && $parte->pase_preop_medico) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_preop_anestesista && ($parte->control_banco_sangre)) {
+//            if ($parte->pase_preop_anestesista && ($parte->pase_banco_sagre)) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_preop_medico && $parte->control_preop_anestesista) {
+//            if ($parte->pase_preop_medico && $parte->pase_preop_anestesista) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_preop_medico && $parte->control_preop_eu) {
+//            if ($parte->pase_preop_medico && $parte->pase_preop_eu) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_preop_medico && $parte->control_banco_sangre) {
+//            if ($parte->pase_preop_medico && $parte->pase_banco_sagre) {
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_banco_sangre && $parte->control_preop_medico  && $parte->control_preop_anestesista &&
+//                                             $parte->control_preop_eu) {
+//            if ($parte->pase_banco_sagre && $parte->pase_preop_medico && $parte->pase_preop_anestesista &&
+//                                             $parte->pase_preop_eu){
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_banco_sangre && $parte->control_preop_medico) {
+//            if ($parte->pase_banco_sagre && $parte->pase_preop_medico){
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_banco_sangre && ($parte->control_preop_anestesista)) {
+//            if ($parte->pase_banco_sagre && ($parte->pase_preop_anestesista)){
+//                $attr = '#99E066';
+//            }
+//        }
+//
+//        if ($parte->control_banco_sangre && ($parte->control_preop_eu)) {
+//            if ($parte->pase_banco_sagre && ($parte->pase_preop_eu)){
+//                $attr = '#99E066';
+//            }
+//        }
 
-        if ($parte->control_preop_eu && ($parte->control_preop_medico)) {
-            if ($parte->pase_preop_eu && ($parte->pase_preop_medico)) {
-                $attr = '#99E066';
-            }
-        }
+//        if ($parte->control_preop_medico) {
+//            if ($parte->pase_preop_medico) {
+//                $attr = '#99E066';
+//            }
+//        }
 
-        if ($parte->control_preop_eu && ($parte->control_banco_sangre)) {
-            if ($parte->pase_preop_eu && ($parte->pase_banco_sagre)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_anestesista && ($parte->control_preop_eu && $parte->control_preop_medico &&
-                                                  $parte->control_banco_sangre)) {
-            if ($parte->pase_preop_anestesista && ($parte->pase_preop_eu && $parte->pase_preop_medico &&
-                                                   $parte->pase_banco_sagre)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_anestesista && ($parte->control_preop_eu)) {
-            if ($parte->pase_preop_anestesista && ($parte->pase_preop_eu)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_anestesista && ($parte->control_preop_medico)) {
-            if ($parte->pase_preop_anestesista && ($parte->pase_preop_medico)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_anestesista && ($parte->control_banco_sangre)) {
-            if ($parte->pase_preop_anestesista && ($parte->pase_banco_sagre)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_medico && ($parte->control_preop_anestesista)) {
-            if ($parte->pase_preop_medico && ($parte->pase_preop_anestesista)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_medico && ($parte->control_preop_eu)) {
-            if ($parte->pase_preop_medico && ($parte->pase_preop_eu)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_medico && ($parte->control_banco_sangre)) {
-            if ($parte->pase_preop_medico && ($parte->pase_banco_sagre)) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_banco_sangre && ($parte->control_preop_medico  && $parte->control_preop_anestesista &&
-                                             $parte->control_preop_eu)) {
-            if ($parte->pase_banco_sagre && ($parte->pase_preop_medico && $parte->pase_preop_anestesista &&
-                                             $parte->pase_preop_eu)){
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_banco_sangre && ($parte->control_preop_medico)) {
-            if ($parte->pase_banco_sagre && ($parte->pase_preop_medico)){
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_banco_sangre && ($parte->control_preop_anestesista)) {
-            if ($parte->pase_banco_sagre && ($parte->pase_preop_anestesista)){
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_banco_sangre && ($parte->control_preop_eu)) {
-            if ($parte->pase_banco_sagre && ($parte->pase_preop_eu)){
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_medico) {
-            if ($parte->pase_preop_medico) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_anestesista) {
-            if ($parte->pase_preop_anestesista) {
-                $attr = '#99E066';
-            }
-        }
-
-        if ($parte->control_preop_eu) {
-            if ($parte->pase_preop_eu) {
-                $attr = '#99E066';
-            }
-        }
+//        if ($parte->control_preop_anestesista) {
+//            if ($parte->pase_preop_anestesista) {
+//                $attr = '#99E066';
+//            }
+//        }
 
         return $attr;
 
