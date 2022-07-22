32a3e48753abc84ef6e7e125e97f1626c1652932
Josue Martinez <m.josue3443@gmail.com>
2022-07-22 00:07:17 -0600

    en perfil admision si esta en lista de espera y entra a editar, que el boton listado regrese a lista de espera

--- resources/views/partes/admision/edit.blade.php
+++ resources/views/partes/admision/edit.blade.php
@@ -15,7 +15,7 @@
                 </div>
                 <div class="col">
                     <a class="btn btn-outline-info float-right"
-                       href="{{route('partes.index')}}">
+                       href="{{ url()->previous() }}">
                         <i class="fa fa-list" aria-hidden="true"></i>&nbsp;<span class="d-none d-sm-inline">{{__('List')}}</span>
                     </a>
                 </div>
