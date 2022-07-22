9f99830ee897379e13d7fd9f71baded520880e7d
Josue Martinez <m.josue3443@gmail.com>
2022-07-21 23:46:53 -0600

    campo dev run esta disabled

--- resources/views/pacientes/fields.blade.php
+++ resources/views/pacientes/fields.blade.php
@@ -26,7 +26,7 @@
     <!-- Dv Run Field -->
     <div class="form-group col-sm-2">
         {!! Form::label('dv_run', 'Dv Run:') !!}
-        {!! Form::text('dv_run', null, ['id' => 'dv_run','class' => 'form-control','maxlength' => 1]) !!}
+        {!! Form::text('dv_run', null, ['id' => 'dv_run','class' => 'form-control','maxlength' => 1,'disabled']) !!}
     </div>
 
     <div class="form-group col-sm-12" style="padding: 0px; margin: 0px"></div>
