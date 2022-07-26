da09bf500ed906b6fded8c349d5688d70dc1b165
Felipe Pino <felipepinoredes@gmail.com>
2022-07-26 15:31:38 -0400

    cambio minimo

--- resources/views/especialidades/edit.blade.php
+++ resources/views/especialidades/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col">
-                    <h1>{{__('Edit Especialida')}}</h1>
+                    <h1>{{__('Editar Especialidad')}}</h1>
                 </div>
                 <div class="col">
                     <a class="btn btn-outline-info float-right"
