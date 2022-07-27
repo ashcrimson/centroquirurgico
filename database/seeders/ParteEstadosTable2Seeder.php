8fb6816918a1b124309091863081bb8964e6162d
Josue Martinez <m.josue3443@gmail.com>
2022-07-27 12:56:05 -0600

    correccion de ParteEstadosTable2Seeder

--- database/seeders/ParteEstadosTable2Seeder.php
+++ database/seeders/ParteEstadosTable2Seeder.php
@@ -14,11 +14,11 @@ class ParteEstadosTable2Seeder extends Seeder
      */
     public function run()
     {
-        /**
-         * @var ParteEstado $parteEstado
-         */
-        $parteEstado = ParteEstado::find(ParteEstado::INGRESADA);
-
-        $parteEstado->update(['nombre' => 'Ingresado']);
+//        /**
+//         * @var ParteEstado $parteEstado
+//         */
+//        $parteEstado = ParteEstado::find(ParteEstado::INGRESADA);
+//
+//        $parteEstado->update(['nombre' => 'Ingresado']);
     }
 }
