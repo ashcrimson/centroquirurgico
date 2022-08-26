53b50380a93c2d28bc25343a3f1fe34f4224a978
Josue Martinez <m.josue3443@gmail.com>
2022-08-26 08:57:35 -0600

    migracion de campos de mantenedor de medicamentos

---
+++ database/migrations/2022_08_26_105632_add_fields3_to_medicamentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFields3ToMedicamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('medicamentos', function (Blueprint $table) {
+            $table->string('reiniciar')->nullable();
+            $table->string('consideraciones')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('medicamentos', function (Blueprint $table) {
+            //
+        });
+    }
+}
